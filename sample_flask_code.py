a4b70aa9bc340f074274746b06ca8500afb49935
abhishekanalytics <abhishekanalytics2023@gmail.com>
2024-01-15 16:52:02 +0530

    sample_flask_code.py

--- sample_flask_code.py
+++ sample_flask_code.py
@@ -1,8 +1,3 @@
-
-
-
-
-
 from flask import Flask, request, jsonify
 import json
 import os
@@ -12,7 +7,7 @@ app = Flask(__name__)
 
 data_file = 'data.json'
 
-@app.route('/tasks', methods=['POST', 'GET'])
+@app.route('/tasks', methods=['POST', 'GET', 'PUT', 'DELETE'])
 def manage_tasks():
     try:
         with open(data_file, 'r') as file:
@@ -24,18 +19,50 @@ def manage_tasks():
         # List all tasks
         return jsonify(tasks)
     elif request.method == 'POST':
-        # Create a new task
+        # Here,Create a new task
         new_task = request.get_json()
         new_task['id'] = str(uuid.uuid4())
         tasks.append(new_task)
 
         with open(data_file, 'w') as file:
-            json.dump(tasks, file,indent=1)
+            json.dump(tasks, file, indent=1)
 
         return jsonify(new_task)
+    elif request.method == 'PUT':
+        #  Here,Update a task
+        task_id_to_update = request.args.get('id')
+        update_data = request.get_json()
+        print("DASDSDFDSFD")
+        for task in tasks:
+            print("ASASASAS")
+            print(task['id'],task_id_to_update)
+
+            if task['id'] == task_id_to_update:
+                task.update(update_data)
+
+                with open(data_file, 'w') as file:
+                    json.dump(tasks, file, indent=1)
+
+                return jsonify(task)
+
+        return jsonify({"error": "Task not found"})
+    
+
+    elif request.method == 'DELETE':
+        # Here,Delete a task
+        task_id_to_delete = request.args.get('id')
+
+        tasks = [task for task in tasks if task['id'] != task_id_to_delete]
+
+        with open(data_file, 'w') as file:
+            json.dump(tasks, file, indent=1)
+
+        return jsonify({"message": "Task deleted successfully"})
 
 if __name__ == '__main__':
+
     app.run(debug=True)
+
 
 
 
